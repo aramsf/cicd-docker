2bc44575c6d18d360f7bea5e12feac0b921ed192
Alexis Ramos <alexis.ramosf@gmail.com>
2020-10-04 21:39:09 +0200

    Create sample project

---
+++ src/test/java/es/arf/cicd/DemoCiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.arf.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
